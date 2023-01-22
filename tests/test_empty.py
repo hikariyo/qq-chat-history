eaa75da833cece01993a11e04e4c02956035d399
kifuan <kifuan@foxmail.com>
2023-01-22 19:02:34 +0800

    test: add test case for empty files

---
+++ tests/test_empty.py
@@ -0,0 +1,22 @@
+from qq_chat_history import Parser
+
+
+lines = '''
+===
+假装我是文件头
+===
+
+其实只要不是时间开头，是什么都无所谓
+
+关注永雏塔菲喵，关注永雏塔菲谢谢喵
+
+嘻嘻喵
+'''.strip().splitlines()
+
+
+def test_empty():
+    parser_group = Parser.get_instance('group')
+    parser_private = Parser.get_instance('private')
+
+    assert not list(parser_group.parse(lines))
+    assert not list(parser_private.parse(lines))
