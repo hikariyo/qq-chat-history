807a10ee8003e73ca02dba10d52f801c323f44f9
kifuan <kifuan@foxmail.com>
2023-01-22 18:35:02 +0800

    feat: add help texts

--- qq_chat_history/cli.py
+++ qq_chat_history/cli.py
@@ -6,13 +6,12 @@ from . import Parser
 
 
 @click.command()
-@click.option('--file-type', '-t', prompt='File type', type=click.Choice(['private', 'group']))
-@click.option('--indent', '-d', prompt='Output indent', type=int, default=2, prompt_required=False)
-@click.option('--input-file', '-i', prompt='Input file path',
+@click.option('--file-type', '-t', prompt='File type', help='The file type.', type=click.Choice(['private', 'group']))
+@click.option('--indent', '-d', prompt='Output indent', help='JSON indent.', type=int, default=2, prompt_required=False)
+@click.option('--input-file', '-i', prompt='Input file path', help='Input file path.',
               type=click.Path(exists=True, dir_okay=False, path_type=Path))
-@click.option('--output-file', '-o', prompt='Output file path',
-              type=click.Path(dir_okay=False, path_type=Path),
-              default='output.json', prompt_required=False)
+@click.option('--output-file', '-o', prompt='Output JSON file path', help='Output JSON file path.',
+              type=click.Path(dir_okay=False, path_type=Path), default='output.json', prompt_required=False)
 def run(input_file: Path, output_file: Path, file_type: str, indent: int):
     parser = Parser.get_instance(file_type)
     lines = input_file.read_text('utf8').splitlines()
