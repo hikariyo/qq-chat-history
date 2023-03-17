6ef8a52ca5b9f2f3e54e20ab1b36da4fe72fb170
kifuan <kifuan@foxmail.com>
2023-03-17 22:19:51 +0800

    chore: make `indent` and `output_file` required for cli

--- qq_chat_history/cli.py
+++ qq_chat_history/cli.py
@@ -5,12 +5,11 @@ from . import parse
 
 
 @click.command()
-@click.option('--indent', '-d', prompt='Output indent', help='Output file indent.', type=int, default=2, prompt_required=False)
+@click.option('--indent', '-d', prompt='Output indent', help='Output file indent.', type=int, default=2)
 @click.option('--input-file', '-i', prompt='Input file path', help='Input file path.',
               type=click.Path(exists=True, dir_okay=False, path_type=Path))
-@click.option('--output-file', '-o', prompt='Output file path(format will be filled automatically)',
-              help='Output file path.', type=click.Path(dir_okay=False, path_type=Path), default='output',
-              prompt_required=False)
+@click.option('--output-file', '-o', prompt='Output file path(will format filled automatically)',
+              help='Output file path.', type=click.Path(dir_okay=False, path_type=Path), default='output')
 @click.option('--output-format', '-f', prompt='Output format', help='Output file format.',
               type=click.Choice(['json', 'yaml']), default='json')
 def run(input_file: Path, output_file: Path, output_format: str, indent: int) -> None:
