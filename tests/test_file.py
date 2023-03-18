a7e0ea4e8aca7d2cbc7b57d6abb9602718f326ad
kifuan <kifuan@foxmail.com>
2023-03-19 01:53:46 +0800

    test: parse file

---
+++ tests/test_file.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from qq_chat_history import Message, parse
+
+file = Path(__file__).parent / 'file.txt'
+
+
+def test_file():
+    body = parse(file)
+
+    assert body.find_first_message_by_id('123123') == Message(
+        id='123123', name='A', content='A', date='1883-03-07 11:22:33',
+    )
+
+    assert body.find_first_message_by_name('B') == Message(
+        id='456456', name='B', content='B', date='1883-03-07 12:34:56',
+    )
