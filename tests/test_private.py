17da6cb20c4593b7e860ff337591438e236bbfba
kifuan <kifuan@foxmail.com>
2023-03-19 00:58:04 +0800

    refactor: inline tests for names and contents

--- tests/test_private.py
+++ tests/test_private.py
@@ -9,7 +9,12 @@ lines = '''
 你说得对
 
 1883-03-07 22:00:51 A
-但是
+2006-01-02 15:04:05
+
+
+
+
+2006-01-02 15:04:05
 '''.strip().splitlines()
 
 
@@ -20,6 +25,7 @@ def test_private() -> None:
         date='1883-03-07 12:34:56', id='B', name='B', content='你说得对',
     )
 
+    # Blank lines will be omitted.
     assert body.find_first_message_by_id('A') == Message(
-        date='1883-03-07 22:00:51', id='A', name='A', content='但是',
+        date='1883-03-07 22:00:51', id='A', name='A', content='2006-01-02 15:04:05\n2006-01-02 15:04:05',
     )
